32c924b9963af62ca57fcf03d6623ee7d190942f
AyaanWebDeveloper <developwithayaan@gmail.com>
2024-09-28 21:23:59 +0500

    My First Javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hi 👋, I'm Ayaanwebdeveloper");
+console.log("A passionate frontend developer from Pakistan");
+console.log("Connect with me:");
+console.log("Github 👇");
+console.log("https://github.com/ayaanwebdeveloper");
+console.log("Linkdein 👇");
+console.log("https://www.linkedin.com/in/ayaan-webdeveloper-a51677323/?trk=opento_sprofile_details");
+console.log("Pls Follow On All Social Media's  account ");
+alert("Hi 👋, I'm Ayaanwebdeveloper")
+alert("Welcome To First Javascript❤❤😃")
